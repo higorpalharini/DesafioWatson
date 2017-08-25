d1e692f8788834d3257f4d4a62fd42b7527a90de
higorpalharini <higorpalharini@outlook.com>
2017-08-25 14:40:26 -0300

    Conversação

--- Conversation_Front-end/js/controllers/conversationCtrl.js
+++ Conversation_Front-end/js/controllers/conversationCtrl.js
@@ -9,11 +9,11 @@ angular.module("conversation").controller("conversationCtrl", function($scope, c
 	}
 
 	$scope.send = function(conversationForm){
-		$scope.conversationForm.response = $scope.conversationForm.response == undefined ? 'Eu: ' + conversationForm.text + '<br>' : $scope.conversationForm.response + '<br>Eu: ' + conversationForm.text + '<br>'; 
+		$scope.conversationForm.response = $scope.conversationForm.response == undefined ? '<span style="float:left;">Eu: ' + conversationForm.text + '</span><br>' : $scope.conversationForm.response + '<br><span style="float:left;">Eu: ' + conversationForm.text + '</span><br>'; 
 
 		conversationAPI.send(conversationForm).success(function (data){
 			$scope.conversationForm.conversation_id = data.conversation_id;
-			$scope.conversationForm.response = $scope.conversationForm.response + '<br> Bot: ' + data.response[0] + '<br>';
+			$scope.conversationForm.response = $scope.conversationForm.response + '<br> <span style="float:right;">Bot: ' + data.response[0] + '</span><br>';
 			limparForm();
 		});
 	};
