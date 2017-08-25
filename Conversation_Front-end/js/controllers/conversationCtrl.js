329b7198c9bf85715c79de724a8b2534369b2efc
higorpalharini <higorpalharini@outlook.com>
2017-08-25 15:27:43 -0300

    Color conversação e alinhamento

--- Conversation_Front-end/js/controllers/conversationCtrl.js
+++ Conversation_Front-end/js/controllers/conversationCtrl.js
@@ -1,4 +1,4 @@
-angular.module("conversation").controller("conversationCtrl", function($scope, conversationAPI){
+angular.module("conversation").controller("conversationCtrl", function($scope, conversationAPI, $sce){
 	
 	$scope.app = "ChatBot";
 	$scope.conversationForm = {};
@@ -9,13 +9,17 @@ angular.module("conversation").controller("conversationCtrl", function($scope, c
 	}
 
 	$scope.send = function(conversationForm){
-		$scope.conversationForm.response = $scope.conversationForm.response == undefined ? '<span style="float:left;">Eu: ' + conversationForm.text + '</span><br>' : $scope.conversationForm.response + '<br><span style="float:left;">Eu: ' + conversationForm.text + '</span><br>'; 
+		$scope.conversationForm.response = $scope.conversationForm.response == undefined ? '<span style="float:right; color: #363636;">Eu: ' + conversationForm.text + '</span>' : $scope.conversationForm.response + '<br><span style="float:right; color: #363636;">Eu: ' + conversationForm.text + '</span>'; 
 
 		conversationAPI.send(conversationForm).success(function (data){
 			$scope.conversationForm.conversation_id = data.conversation_id;
-			$scope.conversationForm.response = $scope.conversationForm.response + '<br> <span style="float:right;">Bot: ' + data.response[0] + '</span><br>';
+			$scope.conversationForm.response = $scope.conversationForm.response + '<br> <span style="float:left; color: #8FBC8F;">Bot: ' + data.response[0] + '</span>';
 			limparForm();
 		});
+	};
+
+	$scope.trustAsHtml = function(string) {
+    	return $sce.trustAsHtml(string);
 	};
 
 });
