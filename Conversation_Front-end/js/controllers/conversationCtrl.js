e3cced2c9b9bca158b750b8b5757312756ebb27c
higorpalharini <higorpalharini@outlook.com>
2017-08-26 14:27:12 -0300

    Alteração de layout e data e hora

--- Conversation_Front-end/js/controllers/conversationCtrl.js
+++ Conversation_Front-end/js/controllers/conversationCtrl.js
@@ -1,7 +1,8 @@
 angular.module("conversation").controller("conversationCtrl", function($scope, conversationAPI, $sce){
 	
-	$scope.app = "ChatBot";
+	$scope.app = "JoBot";
 	$scope.conversationForm = {};
+	$scope.conversationForm.response = "";
 
 	var limparForm = function(){
 		$scope.conversationForm.text = "";
@@ -9,11 +10,13 @@ angular.module("conversation").controller("conversationCtrl", function($scope, c
 	}
 
 	$scope.send = function(conversationForm){
-		$scope.conversationForm.response = $scope.conversationForm.response == undefined ? '<span style="float:right; color: #363636;">Eu: ' + conversationForm.text + '</span>' : $scope.conversationForm.response + '<br><span style="float:right; color: #363636;">Eu: ' + conversationForm.text + '</span>'; 
-
+		$scope.conversationForm.response +=  '<span style="float:right; font-size:10px;">' + getFormatedDate(new Date())  + '</span>';
+		$scope.conversationForm.response += '<span class="label label-default" style="float:right; font-size:14px; margin-bottom: 7px;">Eu: ' + conversationForm.text + '</span><br>'; 
+		
 		conversationAPI.send(conversationForm).success(function (data){
 			$scope.conversationForm.conversation_id = data.conversation_id;
-			$scope.conversationForm.response = $scope.conversationForm.response + '<br> <span style="float:left; color: #8FBC8F;">Bot: ' + data.response[0] + '</span>';
+			$scope.conversationForm.response += '<span class="label label-info" style="float:left; font-size:14px;">JoBot: ' + data.response[0] + '</span>';
+			$scope.conversationForm.response += '<span style="float:left; font-size:10px;">' + getFormatedDate(new Date())  + '</span><br>';
 			limparForm();
 		});
 	};
@@ -21,5 +24,13 @@ angular.module("conversation").controller("conversationCtrl", function($scope, c
 	$scope.trustAsHtml = function(string) {
     	return $sce.trustAsHtml(string);
 	};
+
+	var getFormatedDate = function(date){
+		return date.getHours() + ":" + padLeft(date.getMinutes(),2) + ":" + padLeft(date.getSeconds(),2);
+	}
+
+	var padLeft = function padLeft(nr, n, str){
+	    return Array(n-String(nr).length+1).join(str||'0')+nr;
+	}
 
 });
